394db0f72bee270cf889784005c575435e3a1e61
谢文胜 <xiewensheng@euet.com.cn>
2017-07-25 16:43:52 +0800

    进行了优化

--- app/src/main/java/com/wensheng/translateanimation/RewardActivity.java
+++ app/src/main/java/com/wensheng/translateanimation/RewardActivity.java
@@ -2,6 +2,7 @@ package com.wensheng.translateanimation;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextUtils;
@@ -24,8 +25,9 @@ public class RewardActivity extends BaseActivity {
     private TextView tv_money;
 
     private TextView tv_modify;
-    private LinearLayout rl_call_message;
+    private LinearLayout rl_prompt_box;
     private LinearLayout ll_main;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +49,7 @@ public class RewardActivity extends BaseActivity {
         tv_money = (TextView) findViewById(R.id.tv_money);
 
         tv_modify = (TextView) findViewById(R.id.tv_modify);
-        rl_call_message = (LinearLayout) findViewById(R.id.rl_call_message);
+        rl_prompt_box = (LinearLayout) findViewById(R.id.rl_prompt_box);
 
         InputFilter[] filters = {new CashierInputFilter()};
         et_input.setFilters(filters);
@@ -56,13 +58,13 @@ public class RewardActivity extends BaseActivity {
 
         ll_main = (LinearLayout) findViewById(R.id.ll_main);
         tv_reward.setClickable(true);
-
     }
 
 
     /**
      * View监听
      */
+ 
     public void initeListener() {
 
         et_input.addTextChangedListener(new TextWatcher() {
@@ -72,7 +74,7 @@ public class RewardActivity extends BaseActivity {
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            public void onTextChanged(final CharSequence s, int start, int before, int count) {
 
                 if (TextUtils.isEmpty(s)) {
                     tv_modify.setVisibility(View.VISIBLE);
@@ -82,15 +84,34 @@ public class RewardActivity extends BaseActivity {
                 } else {
                     tv_modify.setVisibility(View.GONE);
                 }
-                if (StringUtils.isBlank(s.toString().trim())) {
-                    tv_reward.setEnabled(false);
-                    tv_money.setText("0.00");
+                if (s.toString().trim().startsWith(".")) {
+                    et_input.setText("0" + s.toString().trim());
+                    tv_money.setText("0" + s.toString().trim());
+                } else if (s.toString().trim().startsWith("0") && s.toString().trim().length() > 1 && !s.toString().trim().contains(".")) {
+                    et_input.setText(s.toString().substring(1, s.toString().trim().length()));
+                    tv_money.setText(s.toString().substring(1, s.toString().trim().length()));
                 } else {
                     if (Float.parseFloat(s.toString().trim()) > 2000) {
                         tv_money.setText("2000");
+                        et_input.setText("2000");
+
+
                         showOrHindeAnimation(true);
+                    } else if (Float.parseFloat(s.toString().trim()) == 2000) {
+
                     } else {
-                        tv_money.setText(s.toString());
+
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        tv_money.setText(s.toString());
+                                    }
+                                });
+                            }
+                        }, 100);
                         showOrHindeAnimation(false);
                     }
                     tv_reward.setEnabled(true);
@@ -114,35 +135,33 @@ public class RewardActivity extends BaseActivity {
     }
 
     /**
-     * 提示单次打赏的动画
-     * @param showOrHinde
+     * 提示框的动画
+     *
+     * @param showOrHinde 显示与隐藏
      */
     public void showOrHindeAnimation(boolean showOrHinde) {
-
         if (showOrHinde) {
-            if (rl_call_message.getVisibility() == View.VISIBLE) {
+            if (rl_prompt_box.getVisibility() == View.VISIBLE) {
                 return;
             } else {
-                rl_call_message.setVisibility(View.VISIBLE);
+                rl_prompt_box.setVisibility(View.VISIBLE);
             }
         } else {
-            if (rl_call_message.getVisibility() != View.VISIBLE) {
+            if (rl_prompt_box.getVisibility() != View.VISIBLE) {
                 return;
             }
         }
-
         TranslateAnimation animation = new TranslateAnimation(
                 Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f,
                 Animation.RELATIVE_TO_SELF, showOrHinde ? 0.0f : 1.0f, Animation.RELATIVE_TO_SELF, showOrHinde ? 1.0f : 0.0f
         );
         animation.setDuration(500);
         animation.setFillAfter(true);
-        rl_call_message.startAnimation(animation);
+        rl_prompt_box.startAnimation(animation);
 
         if (!showOrHinde) {
-            rl_call_message.setVisibility(View.GONE);
+            rl_prompt_box.setVisibility(View.GONE);
         }
     }
-
 
 }
